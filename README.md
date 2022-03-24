0dea8af878fa2b92173e60b2688a33efe65001ce
Daniel Wisky <daniel-ddw@hotmail.com>
2022-03-24 14:36:05 -0300

    add unit test

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # friends
 
-basic friend registration
+crud kotlin + spring + mongodb
+
+### Reference Documentation
+
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.6.5/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.6.5/maven-plugin/reference/html/#build-image)
+* [Spring Data MongoDB](https://docs.spring.io/spring-boot/docs/2.6.5/reference/htmlsingle/#boot-features-mongodb)
+* [Validation](https://docs.spring.io/spring-boot/docs/2.6.5/reference/htmlsingle/#boot-features-validation)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/2.6.5/reference/htmlsingle/#boot-features-developing-web-applications)
+
+### Guides
+
+The following guides illustrate how to use some features concretely:
+
+* [Accessing Data with MongoDB](https://spring.io/guides/gs/accessing-data-mongodb/)
+* [Validation](https://spring.io/guides/gs/validating-form-input/)
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
---
+++ src/test/kotlin/br/com/danielwisky/friends/gateways/outputs/mongodb/FriendDataGatewayImplTest.kt
@@ -0,0 +1,83 @@
+package br.com.danielwisky.friends.gateways.outputs.mongodb
+
+import br.com.danielwisky.friends.UnitTest
+import br.com.danielwisky.friends.domains.FriendFilter
+import br.com.danielwisky.friends.gateways.outputs.mongodb.documents.FriendDocument
+import br.com.danielwisky.friends.gateways.outputs.mongodb.repositories.FriendCustomRepository
+import br.com.danielwisky.friends.gateways.outputs.mongodb.repositories.FriendRepository
+import br.com.danielwisky.friends.templates.FriendTemplate
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import java.util.*
+
+class FriendDataGatewayImplTest : UnitTest() {
+
+    @InjectMockKs
+    lateinit var friendDataGateway: FriendDataGatewayImpl
+
+    @MockK
+    lateinit var friendRepository: FriendRepository
+
+    @MockK
+    lateinit var friendCustomRepository: FriendCustomRepository
+
+    @Test
+    fun `should save`() {
+        val friend = FriendTemplate.valid()
+        val friendDocument = FriendDocument(friend)
+
+        every { friendRepository.save(friendDocument) } returns friendDocument
+
+        val friendSaved = friendDataGateway.save(friend)
+
+        assertNotNull(friendSaved)
+    }
+
+    @Test
+    fun `should find`() {
+        val friend = FriendTemplate.valid()
+        val friendDocument = FriendDocument(friend)
+        val id = friendDocument.id!!
+
+        every { friendRepository.findById(id) } returns Optional.of(friendDocument)
+
+        val friendReturned = friendDataGateway.findById(id)
+
+        assertTrue(friendReturned.isPresent)
+    }
+
+    @Test
+    fun `should return empty when friend not found`() {
+        val friend = FriendTemplate.valid()
+        val id = friend.id!!
+
+        every { friendRepository.findById(id) } returns Optional.empty()
+
+        val friendReturned = friendDataGateway.findById(id)
+
+        assertFalse(friendReturned.isPresent)
+    }
+
+    @Test
+    fun `should search`() {
+        val friend = FriendTemplate.valid()
+        val filter = FriendFilter()
+        val pageable = PageRequest.of(0, 20)
+
+        every { friendCustomRepository.search(filter, pageable) } returns PageImpl(
+            mutableListOf(
+                FriendDocument(friend)
+            )
+        )
+
+        val friends = friendDataGateway.search(filter, pageable)
+
+        assertNotNull(friends)
+        assertEquals(friends.totalElements, 1)
+    }
+}
